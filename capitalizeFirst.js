5b8fe96a39a8a49a8daa4c13c4a093b0521df1a7
Cécile Dubois <64218310+DuboisCecile@users.noreply.github.com>
2021-03-20 16:26:01 +0100

    Delete capitalizeFirst.js

--- capitalizeFirst.js
+++
@@ -1,6 +0,0 @@
-function capitalizeFirst(input) {
-    const capInput = input.split(' ').map(element => element.charAt(0).toUpperCase() + element.substring(1));
-    return capInput.join(' ');
-}
-
-module.exports = capitalizeFirst;
